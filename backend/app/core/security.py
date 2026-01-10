301ff954eb68a14983ec4d475cda2d6479750bb8
jeffreyesedo <jiesedo@gmail.com>
2026-01-11 00:48:56 +0100

    security file

---
+++ backend/app/core/security.py
@@ -0,0 +1,6 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from app.core.config import settings
+
+security = HTTPBearer()
