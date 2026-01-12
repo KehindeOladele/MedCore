8634954167841048555e8f18969906c9f98ca826
jeffreyesedo <jiesedo@gmail.com>
2026-01-12 19:39:15 +0100

    supabase client

--- backend/app/core/supabase_client.py
+++ backend/app/core/supabase_client.py
@@ -1,6 +1,8 @@
 from supabase import create_client, Client
 from app.core.config import settings
 
+
+# ---- Initialize Supabase Client -----
 supabase: Client = create_client(
     settings.SUPABASE_URL,
     settings.SUPABASE_SERVICE_ROLE_KEY
