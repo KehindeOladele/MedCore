807ff66f2443ed935e2eddca22fbef415e10fd41
jeffreyesedo <jiesedo@gmail.com>
2026-01-14 17:43:28 +0100

    Update: ensure_profile_exists func user_id and user_email statement

--- backend/app/modules/auth/service.py
+++ backend/app/modules/auth/service.py
@@ -1,17 +1,36 @@
 from app.core.supabase_client import supabase
 
 
+# ----- Siugnup Service [email & password] -----
+def sign_up(email: str, password: str):
+    
+    # ----- Create user in Supabase Auth -----
+    response = supabase.auth.sign_up({
+        "email": email,
+        "password": password
+    })
+    
+    # ----- Return Response -----
+    user = response.user
+    if not user:
+        return {"error": response.get("error")}
+
+    # ----- Return user info -----
+    return user
+
+
 # ----- Ensure User Profile Exists -----
-def ensure_profile_exists(user: dict):
+def ensure_profile_exists(user):
     """
     Ensure a profiles row exists for authenticated users. Sync Supabase Auth → DB
 
     input: user (dict) from get_current_user
     Returns: None
     """
-    user_id = user["id"]
+    user_id = user["id"] if isinstance(user, dict) else user.id
+    user_email= user["email"] if isinstance(user, dict) else user.email
 
-    result = (
+    response = (
         supabase
         .table("profiles")
         .select("id")
@@ -19,11 +38,12 @@ def ensure_profile_exists(user: dict):
         .execute()
     )
 
-    if result.data:
+    if response.data:
         return
 
     # ----- Create New Profile -----
     supabase.table("profiles").insert({
         "id": user_id,
-        "role": user["role"],
+        "email": user_email,
+        "role": "patient",
     }).execute()
