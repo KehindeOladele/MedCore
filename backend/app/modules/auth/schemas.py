c0048c34bee9e5e26b8cbb32544734edef336180
jeffreyesedo <jiesedo@gmail.com>
2026-01-14 17:41:54 +0100

    Update: id - UUID and email - EmailStr

--- backend/app/modules/auth/schemas.py
+++ backend/app/modules/auth/schemas.py
@@ -1,9 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from uuid import UUID
 
 
 # ----- UserMe Schema -----
 class UserMe(BaseModel):
     id: UUID
-    email: str
+    email: EmailStr
+    role: str
+
+
+# ----- SignupRequest Schema -----
+class SignupRequest(BaseModel):
+    email: EmailStr
+    password: str
+    # full_name: str = None
+
+
+# ----- SignupResponse Schema -----
+class SignupResponse(BaseModel):
+    id: UUID
+    email: EmailStr
     role: str
