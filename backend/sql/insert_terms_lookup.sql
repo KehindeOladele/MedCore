f9b266eab5575064c1094ee601d2a09c9d83dc99
jeffreyesedo <jiesedo@gmail.com>
2026-01-15 11:26:46 +0100

    Insert terminology codes, systems, and display to table

---
+++ backend/sql/insert_terms_lookup.sql
@@ -0,0 +1,6 @@
+-- Insert Terminology Codes
+insert into public.terminology_codes (system, code, display)
+values
+('LOINC', '8480-6', 'Systolic blood pressure'),
+('SNOMED', '44054006', 'Diabetes mellitus type 2'),
+('RxNorm', '860975', 'Metformin 500 MG Oral Tablet');
