0497edc490c74c92b50e3bc2f47b2f73ecbf1d61
jeffreyesedo <jiesedo@gmail.com>
2026-01-17 13:11:44 +0100

    Update: Add records router to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.modules.auth.router import router as auth_router
 from app.modules.patients.router import router as patients_router
+from app.modules.records.router import router as records_router
 
 
 # ----- Initialize FastAPI Application -----
@@ -8,10 +9,16 @@ app = FastAPI(title="MedCore API")
 
 
 # ----- Include Routers -----
+
+# ----- Auth Router -----
 app.include_router(auth_router)
 
 # ---- Patients Router -----
 app.include_router(patients_router)
+
+# ---- Records Router -----
+app.include_router(records_router)
+
 
 # ----- Health Check Endpoint -----
 @app.get("/health")
