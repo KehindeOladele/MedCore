1b963d3bee250f27536b34d03aeafe524bfdc3e8
jeffreyesedo <jiesedo@gmail.com>
2026-01-19 17:51:49 +0100

    Update: create_condition, create_observation, create_mediation return create_record

--- backend/app/modules/records/router.py
+++ backend/app/modules/records/router.py
@@ -3,39 +3,37 @@ from app.core.security import get_current_user
 from app.modules.records.service import create_record
 from app.modules.records.models import MedicalRecordCreate
 from app.modules.records.models import ObservationCreate, ConditionCreate
-from app.modules.records.service import create_observation, create_condition
 from app.core.security import require_permission
 
 
 router = APIRouter(prefix="/records", tags=["Medical Records"])
 
 
-# ----- Create Medical Record Endpoint -----
-@router.post("/", status_code=201)
-def create_medical_record(
-    payload: MedicalRecordCreate,
-    current_user=Depends(get_current_user)
-):
-    """
-    Create a medical record (FHIR Observation / Condition)
-    """
-    if current_user["role"] not in ["clinician", "doctor", "admin"]: 
-        raise HTTPException(status_code=403, detail="Not authorized")
-
-    record = create_record(payload, clinician_id=current_user["id"])
-    return {"status": "created", "record": record}
-
-
 # ----- Create Observation Record Endpoint-----
 @router.post("/observations", status_code=201)
-def create_observation_api(
-    payload: ObservationCreate,
-    current_user=Depends(get_current_user)
+def create_observation(
+    payload: MedicalRecordCreate,
+    current_user=Depends(require_permission("create_observation"))
 ):
-    if current_user["role"] not in ["clinician", "doctor", "admin"]:
-        raise HTTPException(status_code=403, detail="Not authorized")
+    if payload.record_type != "observation":
+        raise HTTPException(400, "record_type must be 'observation'")
 
-    return create_observation(payload, current_user["id"])
+    return create_record(payload, clinician_id=current_user["id"])
+
+
+
+# ----- Create Condition Record Endpoint-----
+@router.post("/conditions", status_code=201)
+def create_condition(
+    payload: MedicalRecordCreate,
+    current_user=Depends(require_permission("create_condition"))
+):
+    if payload.record_type != "condition":
+        raise HTTPException(400, "record_type must be 'condition'")
+
+    return create_record(payload, clinician_id=current_user["id"])
+
+
 
 
 # ----- Create Condition Record with RBAC Endpoint-----
@@ -48,3 +46,4 @@ def create_medication(
         raise HTTPException(400, "record_type must be 'medication'")
 
     return create_record(payload, clinician_id=current_user["id"])
+
