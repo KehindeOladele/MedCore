b0bbb5cbf9fbaf778763a1358de4de95fafeb363
jeffreyesedo <jiesedo@gmail.com>
2026-01-27 10:25:09 +0100

    insert permissions into permissions table

--- backend/sql/insert_role_permission_to_permission.sql
+++ backend/sql/insert_role_permission_to_permission.sql
@@ -4,3 +4,11 @@ values (
     'resolve_condition',
     'Resolve an active patient condition'
 );
+
+
+insert into permissions (id, name, description)
+values
+    (gen_random_uuid(), 'create_observation', 'Create observation records'),
+    (gen_random_uuid(), 'create_condition', 'Create condition records'),
+    (gen_random_uuid(), 'view_patient_qr', 'View patient QR code'),
+    (gen_random_uuid(), 'view_patient_fhir', 'View patient FHIR bundle');
