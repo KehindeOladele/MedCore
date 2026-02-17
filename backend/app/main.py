7cb81e5d970f808fc153cf8ec5d71806b093a8ab
jeffreyesedo <jiesedo@gmail.com>
2026-02-17 13:04:08 +0100

    remove prescription router

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,6 @@ from app.modules.records.router import router as records_router
 from app.modules.dashboard.router import router as dashboard_router
 from app.modules.medical_history.router import router as medical_history_router
 from app.modules.laboratory.router import router as laboratory_router
-from app.modules.prescriptions.router import router as prescriptions_router
 
 
 # ===== Initialize FastAPI Application =====
@@ -29,8 +28,7 @@ app.include_router(records_router)
 app.include_router(medical_history_router)
 # Laboratory Router
 app.include_router(laboratory_router)
-# Prescriptions Router
-app.include_router(prescriptions_router)
+
 
 # ===== Aggregation Layer =====
 app.include_router(dashboard_router)
