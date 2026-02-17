a53d5c262c55765405ded4a10f9099364ddd0ba9
jeffreyesedo <jiesedo@gmail.com>
2026-02-17 11:42:45 +0100

    dashboard, medical_history, reminders, labs, prescriptions routers  to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,23 +2,41 @@ from fastapi import FastAPI
 from app.modules.auth.router import router as auth_router
 from app.modules.patients.router import router as patients_router
 from app.modules.records.router import router as records_router
+from app.modules.dashboard.router import router as dashboard_router
+from app.modules.medicacl_history.router import router as medical_history_router
+from app.modules.reminders.router import router as reminders_router
+from app.modules.laboratory.router import router as laboratory_router
+from app.modules.prescriptions.router import router as prescriptions_router
 
 
-# ----- Initialize FastAPI Application -----
+# ===== Initialize FastAPI Application =====
 app = FastAPI(title="MedCore API")
 
 
 # ===== Include Routers =====
 
-# ----- Auth Router -----
+
+#  ===== Core Modules =====
+# Auth Router
 app.include_router(auth_router)
-
-# ---- Patients Router -----
+# Patients Router
 app.include_router(patients_router)
-
-# ---- Records Router -----
+# Records Router
 app.include_router(records_router)
 
+
+# ===== Feature Modules =====
+# Medical History Router
+app.include_router(medical_history_router)
+# Reminders Router
+app.include_router(reminders_router)
+# Laboratory Router
+app.include_router(laboratory_router)
+# Prescriptions Router
+app.include_router(prescriptions_router)
+
+# ===== Aggregation Layer =====
+app.include_router(dashboard_router)
 
 # ----- Health Check Endpoint -----
 @app.get("/health")
