5d7dc8d75b1ce647933a55e060374d6aaa4fb3dc
jeffreyesedo <jiesedo@gmail.com>
2026-02-18 01:12:35 +0100

    get minimal payload for qrcode

--- backend/app/shared/utils/qr.py
+++ backend/app/shared/utils/qr.py
@@ -4,8 +4,12 @@ from io import BytesIO
 
 # ----- QR Code Generation Utility -----
 def generate_qr(data: dict):
-    payload = json.dumps(data, separators=(",", ":"))
-    qr = qrcode.make(payload)
+    minimal_payload = {
+        "type": "patient",
+        "id": patient_id,
+        "v": 1
+    }
+    qr = qrcode.add_data(json.dumps(minimal_payload))
     buf = BytesIO()
     qr.save(buf, format="PNG")
     buf.seek(0)
