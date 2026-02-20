3bc9bb3bcb89e6ca9bba332e6802ef912a56f104
jeffreyesedo <jiesedo@gmail.com>
2026-02-20 23:58:38 +0100

    add update organization endpoint

--- backend/app/modules/organizations/router.py
+++ backend/app/modules/organizations/router.py
@@ -3,8 +3,14 @@ from app.modules.organizations.models import (
     OrganizationCreate,
     OrganizationUpdate
 )
-from app.modules.organizations.service import create_organization
-from app.core.security import get_current_user
+from app.modules.organizations.service import (
+    create_organization,
+    update_organization
+)
+from app.core.security import (
+    get_current_user,
+    require_permission
+)
 from app.core.supabase_client import supabase
 
 
@@ -86,3 +92,26 @@ async def upload_logo(
         "message": "Logo uploaded successfully",
         "logo_url": public_url
     }
+
+
+
+# ---- Update Organization Endpoint -----
+@router.put("/update")
+def update_my_organization(
+    payload: OrganizationUpdate,
+    current_user=Depends(require_permission("manage_organization"))
+):
+
+    # get org id from admin table
+    admin_record = (
+        supabase
+        .table("admins")
+        .select("organization_id")
+        .eq("id", current_user["id"])
+        .single()
+        .execute()
+    )
+
+    org_id = admin_record.data["organization_id"]
+
+    return update_organization(org_id, payload)
