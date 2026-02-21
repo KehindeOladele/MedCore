63f388fc97869402f84e8bcec94d882f5f6343ff
jeffreyesedo <jiesedo@gmail.com>
2026-02-21 12:52:44 +0100

    add description column to roles table

--- backend/sql/user_roles.sql
+++ backend/sql/user_roles.sql
@@ -1,5 +1,6 @@
 -- SQL script to create a roles table
 create table roles (
     id uuid primary key default gen_random_uuid(),
-    name text unique not null
+    name text unique not null,  -- org_admin, practitioners, staff, super_admin 
+    description text
 );
