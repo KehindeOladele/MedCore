8142f39106ac17b3996480b091ff52031f077718
jeffreyesedo <jiesedo@gmail.com>
2026-02-21 13:11:39 +0100

    add columns organization_id, role_type, is_active

--- backend/sql/user_roles.sql
+++ backend/sql/user_roles.sql
@@ -1,6 +1,11 @@
 -- SQL script to create a roles table
 create table roles (
     id uuid primary key default gen_random_uuid(),
-    name text unique not null,  -- org_admin, practitioners, staff, super_admin 
+    name text unique (name, organization_id) not null,  -- org_admin, practitioners, staff, super_admin 
+    organization_id uuid references organization(id) on delete cascade,
+    role_type text check (role_type in ('system', 'organization')) default 'organization',
+    is_active boolean default true,
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
     description text
 );
