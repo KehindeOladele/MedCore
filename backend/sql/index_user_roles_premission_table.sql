69cce3f54aeef30bd44b362d3e18900d1ef34cd2
jeffreyesedo <jiesedo@gmail.com>
2026-02-21 13:12:16 +0100

    Add Indexes use_roles and role_permissions

--- backend/sql/index_user_roles_premission_table.sql
+++ backend/sql/index_user_roles_premission_table.sql
@@ -1,4 +1,9 @@
 -- Indexes for user_roles and role_permissions tables
+-- Your app will frequently query:
+-- user roles by user_id
+-- roles per organization
+-- permissions per role
+
 create index idx_user_roles_user on public.user_roles(user_id);
 create index idx_user_roles_org on public.user_roles(organization_id);
 create index idx_role_permissions_role on role_permissions(role_id);
