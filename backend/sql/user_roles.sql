485f4b874dfd8f1c2ef9c295aec5be867143f141
jeffreyesedo <jiesedo@gmail.com>
2026-02-21 13:23:45 +0100

    correct query add unique name and org

--- backend/sql/user_roles.sql
+++ backend/sql/user_roles.sql
@@ -1,11 +1,12 @@
 -- SQL script to create a roles table
 create table roles (
     id uuid primary key default gen_random_uuid(),
-    name text unique (name, organization_id) not null,  -- org_admin, practitioners, staff, super_admin 
+    name text not null,
     organization_id uuid references organization(id) on delete cascade,
     role_type text check (role_type in ('system', 'organization')) default 'organization',
     is_active boolean default true,
     created_at timestamptz default now(),
     updated_at timestamptz default now(),
-    description text
+    description text,
+    unique (name, organization_id)
 );
