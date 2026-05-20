667aa77014f5a6f35570d643d0752ab65b9dc132
jeffreyesedo <jiesedo@gmail.com>
2026-05-20 14:37:16 +0100

    removed docs folder and website files

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.getElementById('navToggle');
-const pageNav = document.getElementById('pageNav');
-
-navToggle?.addEventListener('click', () => {
-  pageNav.classList.toggle('is-open');
-});
