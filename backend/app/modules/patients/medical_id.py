2c0e2a40fac20d359dcb3f140b39ee1417375a19
jeffreyesedo <jiesedo@gmail.com>
2026-05-26 20:25:57 +0100

    utility to create and inssert medical -d

---
+++ backend/app/modules/patients/medical_id.py
@@ -0,0 +1,16 @@
+from app.core.supabase_admin import supabase_admin
+
+
+# Create and Insert Medical_id to Existing user
+def generate_medical_id():
+
+    result = (
+        supabase_admin
+        .table("patients")
+        .select("*", count="exact")
+        .execute()
+    )
+
+    next_number = (result.count or 0) + 1
+
+    return f"MC-NG-{next_number:08d}"
