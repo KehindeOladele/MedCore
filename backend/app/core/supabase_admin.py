68c1fb3df11eb5577dac32b851a86517a507c8c0
jeffreyesedo <jiesedo@gmail.com>
2026-05-27 18:48:17 +0100

    modified code to reflect Supabase change

--- backend/app/core/supabase_admin.py
+++ backend/app/core/supabase_admin.py
@@ -2,6 +2,6 @@ from supabase import create_client
 import os
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_SECRET_KEY = os.getenv("SUPABASE_SECRET_KEY")
 
-supabase_admin = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+supabase_admin = create_client(SUPABASE_URL, SUPABASE_SECRET_KEY)
