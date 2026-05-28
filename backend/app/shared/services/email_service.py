366ee84b553d3b3dfcfde589f429007dfc8cdc44
jeffreyesedo <jiesedo@gmail.com>
2026-05-28 15:58:50 +0100

    update service to use email_service class

--- backend/app/shared/services/email_service.py
+++ backend/app/shared/services/email_service.py
@@ -1,5 +1,6 @@
 import resend
 from app.core.config import settings
+from app.shared.schemas.email_service import EmailService
 
 
 # ----- Initial Resend API -----
@@ -7,15 +8,11 @@ resend.api_key = settings.RESEND_API_KEY
 
 
 # ----- Send Email Service -----
-def send_email(
-    to_email: str,
-    subject: str,
-    html: str
-):
+def send_email(email_service: EmailService):
 
     return resend.Emails.send({
         "from": settings.EMAIL_FROM,
-        "to": [to_email],
-        "subject": subject,
-        "html": html
+        "to": [email_service.to],
+        "subject": email_service.subject,
+        "html": email_service.html
     })
