6dedcabdd560388e65c8eee9362b1055748ac132
jeffreyesedo <jiesedo@gmail.com>
2026-05-28 21:57:06 +0100

    strip to remove tailing whitespace

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,11 +11,11 @@ class Settings:
 
 # ----- Supabase Configuration -----
     # 1. Supabase PRoject URL
-    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL").strip()
     # 2. Public/User-scoped
-    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY") # SUPABASE_KEY : Client-side (browser/mobile)
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY").strip() # SUPABASE_KEY : Client-side (browser/mobile)
     # 3. Backend/Admin-scoped
-    SUPABASE_SECRET_KEY: str = os.getenv("SUPABASE_SECRET_KEY") # SUPABASE_SECRET_KEY: Server-side (Edge Functions / your backend only)
+    SUPABASE_SECRET_KEY: str = os.getenv("SUPABASE_SECRET_KEY").strip() # SUPABASE_SECRET_KEY: Server-side (Edge Functions / your backend only)
 
     # ----- Security -----
     JWT_AUDIENCE: str = os.getenv("JWT_AUDIENCE", "authenticated")
