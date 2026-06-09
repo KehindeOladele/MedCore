56e6d40f2c07cff0f3aa67ea2f17e246427cd07b
jeffreyesedo <jiesedo@gmail.com>
2026-06-09 13:37:07 +0100

    onboarding email hander

---
+++ backend/app/modules/patients/event_hanlders.py
@@ -0,0 +1,35 @@
+from app.core.events.emitter import emit_event
+from app.core.events.schemas import EventTypes
+from app.modules.patients.onboarding import send_onboarding_email
+
+
+# ----- Patient Create Hanlder ----- 
+def handle_patient_created(
+    patient_id: str,
+    email: str
+):
+
+    emit_event(
+        aggregate_type="patient",
+        aggregate_id=patient_id,
+        event_type=EventTypes.ONBOARDING_EMAIL_REQUESTED,
+        payload={
+            "email": email
+        }
+    )
+
+
+# ----- Onboarding Email Request Handler -----
+def handle_onboarding_email_requested(
+    patient_id: str
+):
+
+    # existing send_onboarding_email logic
+
+    send_onboarding_email(patient_id)
+
+    emit_event(
+        aggregate_type="patient",
+        aggregate_id=patient_id,
+        event_type=EventTypes.ONBOARDING_EMAIL_SENT
+    )
