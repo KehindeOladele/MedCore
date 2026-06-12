0a82fec50aebdecbe12a13e35ca6973345bc1385
jeffreyesedo <jiesedo@gmail.com>
2026-06-12 22:44:27 +0100

    refactor to get event type and check handlers and event type

--- backend/app/core/events/dispatcher.py
+++ backend/app/core/events/dispatcher.py
@@ -1,14 +1,37 @@
 from app.core.events.registry import EVENT_HANDLERS
+from app.core.events.exceptions import (
+    EventProcessingError,
+    EventHandlerNotFoundError
+)
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 #  ----- Event Dispatcher -----
 def dispatch_event(event: dict):
 
-    event_type = event["event_type"]
+    event_type = event.get("event_type")
 
+    if not event_type:
+        raise EventProcessingError(
+            "Missing event_type"
+        )
+    
     handler = EVENT_HANDLERS.get(event_type)
 
     if not handler:
-        return
+        raise EventHandlerNotFoundError(
+            f"No handler registered for "
+            f"event type '{event_type}'"
+        )
+
+    logger.info(
+        "Dispatching event %s (%s)",
+        event["id"],
+        event_type
+    )
+
+
 
     handler(event)
