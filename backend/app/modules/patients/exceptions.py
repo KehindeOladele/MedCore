a3e6c460c01e4b95d1a8e99428e84eeb9ae4aa51
jeffreyesedo <jiesedo@gmail.com>
2026-06-15 17:13:02 +0100

    exceptions class for patient module error

--- backend/app/modules/patients/exceptions.py
+++ backend/app/modules/patients/exceptions.py
@@ -1,3 +1,33 @@
 # ----- Delivery Response Model -----
 class EmailDeliveryError(Exception):
+    """Email Fialed to Deliver."""
+    pass
+
+# ----- Patient Error Model -----
+class PatientError(Exception):
+    """Base patient exception."""
+    pass
+
+
+# ----- Patient Not Found Error Model -----
+class PatientNotFoundError(PatientError):
+    """Patient does not exist."""
+    pass
+
+
+# ----- Patient Create Error Model -----
+class PatientCreationError(PatientError):
+    """Failed to create patient."""
+    pass
+
+
+# ----- Patient Update Error Model -----
+class PatientUpdateError(PatientError):
+    """Failed to update patient."""
+    pass
+
+
+# ----- Clinician Assignemnt Error Model -----
+class ClinicianAssignmentError(PatientError):
+    """Failed clinician assignment."""
     pass
