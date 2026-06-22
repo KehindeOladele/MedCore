159c36df95d78c5204d174d77491013cb055a325
jeffreyesedo <jiesedo@gmail.com>
2026-06-22 23:37:01 +0100

    update events_dead_letter table query

--- backend/sql/events/DLQ_Table.sql
+++ backend/sql/events/DLQ_Table.sql
@@ -12,5 +12,9 @@ CREATE TABLE IF NOT EXISTS events_dead_letter (
     retry_count INT,
     failure_reason TEXT,
 
+    replayed BOOLEAN DEFAULT FALSE,
+    replayed_at TIMESTAMPTZ,
+    replayed_by UUID,
+
     failed_at TIMESTAMPTZ DEFAULT NOW()
 );
