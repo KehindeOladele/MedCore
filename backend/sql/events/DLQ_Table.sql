23cc0bdef9437235c9a34cab601f63e3e7e8ceab
jeffreyesedo <jiesedo@gmail.com>
2026-06-23 00:57:12 +0100

    update code for columns to be not null

--- backend/sql/events/DLQ_Table.sql
+++ backend/sql/events/DLQ_Table.sql
@@ -1,19 +1,19 @@
 -- Dead Letter Queue (DLQ) Table
 -- Table for Dead events
 CREATE TABLE IF NOT EXISTS events_dead_letter (
-    id UUID PRIMARY KEY,
-    original_event_id UUID,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    original_event_id UUID NOT NULL,
     
-    aggregate_type TEXT,
-    aggregate_id TEXT,
-    event_type TEXT,
+    aggregate_type TEXT NOT NULL,
+    aggregate_id TEXT NOT NULL,
+    event_type TEXT NOT NULL,
     payload JSONB,
 
-    retry_count INT,
-    failure_reason TEXT,
+    retry_count INT NOT NULL,
+    failure_reason TEXT NOT NULL,
 
-    replayed BOOLEAN DEFAULT FALSE,
-    replayed_at TIMESTAMPTZ,
+    replayed BOOLEAN DEFAULT FALSE NOT NULL,
+    replayed_at TIMESTAMPTZ NOT NULL,
     replayed_by UUID,
 
     failed_at TIMESTAMPTZ DEFAULT NOW()
