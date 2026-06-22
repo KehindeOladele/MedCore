9569d7738bc4acb7e3dc965c3e71609eda90b8f5
jeffreyesedo <jiesedo@gmail.com>
2026-06-22 22:30:17 +0100

    Refactor code to create Audit_logs table

--- backend/sql/Audit_Table/audit_table.sql
+++ backend/sql/Audit_Table/audit_table.sql
@@ -1,9 +1,20 @@
-create table audit_logs (
-    id uuid default gen_random_uuid(),
-    user_id uuid,
-    action text,
-    resource text,
-    resource_id uuid,
-    metadata jsonb,
-    created_at timestamptz default now()
+-- Query to Create Audit table
+CREATE TABLE IF NOT EXISTS audit_logs (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    actor_id UUID,
+    actor_type TEXT,
+
+    action TEXT NOT NULL,
+
+    resource_type TEXT NOT NULL,
+    resource_id TEXT NOT NULL,
+
+    metadata JSONB DEFAULT '{}'::jsonb,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+-- Add Supabase Row Level Security
+ALTER TABLE audit_logs
+ENABLE ROW LEVEL SECURITY;
