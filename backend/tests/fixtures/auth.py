b132f8f0c1c1aeb7ef3dd23cb6aa23d712e40f50
jeffreyesedo <jiesedo@gmail.com>
2026-07-27 14:57:39 +0100

    calling user factory properly

--- backend/tests/fixtures/auth.py
+++ backend/tests/fixtures/auth.py
@@ -1,31 +1,20 @@
 import pytest
-
 from fastapi.testclient import TestClient
-
 from main import app
 from app.core.security import get_current_user
-
 from tests.factories.user import user_factory
+
+
 
 @pytest.fixture
 def current_user():
-
     return user_factory()
 
 
 @pytest.fixture
-def authenticated_client(
-    current_user,
-):
+def authenticated_client(current_user):
 
-    app.dependency_overrides[
-        get_current_user
-    ] = (
-        lambda: current_user
-    )
-
+    app.dependency_overrides[get_current_user] = lambda: current_user
     client = TestClient(app)
-
     yield client
-
     app.dependency_overrides.clear()
