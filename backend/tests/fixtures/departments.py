cd2ef52241344f727ee7b9a6afa4fa250ff664f8
jeffreyesedo <jiesedo@gmail.com>
2026-07-31 18:45:28 +0100

    add create department fixure

--- backend/tests/fixtures/departments.py
+++ backend/tests/fixtures/departments.py
@@ -1,5 +1,9 @@
 import pytest
 from tests.factories.departments import department_factory
+from app.modules.organizations.departments.schemas import (
+    DepartmentCreate,
+    DepartmentResponse,
+)
 
 
 @pytest.fixture
@@ -7,9 +11,25 @@ def department_data():
     return department_factory()
 
 
+# -----------------
+# UPDATE DEPARTMENT   
+# -----------------
 @pytest.fixture
 def updated_department_data():
     return department_factory(
         name="Updated Cardiology",
         description="Updated description",
+    )
+
+
+# -----------------
+# CREATE DEPARTMENT   
+# -----------------
+@pytest.fixture
+def create_payload():
+    return DepartmentCreate(
+        name="Cardiology",
+        code="CARD",
+        description="Cardiology Department",
+        parent_department_id=None,
     )
