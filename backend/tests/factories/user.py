ebacb75347a68e72fc8ad242e0b86f5373d3e651
jeffreyesedo <jiesedo@gmail.com>
2026-08-04 00:26:27 +0100

    refactor factory data

--- backend/tests/factories/user.py
+++ backend/tests/factories/user.py
@@ -2,9 +2,11 @@ from copy import deepcopy
 
 
 _DEFAULT_USER = {
-    "id": "user-123",
-    "email": "admin@test.com",
-}
+        "id": "user-123",
+        "email": "admin@test.com",
+        "role": "admin",
+        "organization_id": "org-123",
+    }
 
 
 def user_factory(**overrides):
