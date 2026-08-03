13feef6607e1c034b9249516d48db334d9e98e68
jeffreyesedo <jiesedo@gmail.com>
2026-08-04 00:27:30 +0100

    refactor fake org value

--- backend/tests/fixtures/auth.py
+++ backend/tests/fixtures/auth.py
@@ -1,13 +1,20 @@
 import pytest
+from uuid import uuid4
 from fastapi.testclient import TestClient
 from main import app
 from app.core.security import get_current_user
 from tests.factories.user import user_factory
+from app.modules.organizations.dependencies import (
+    require_organization_admin,
+    require_organization_member,
+)
 
 
 
 @pytest.fixture
 def client():
+    app.dependency_overrides.clear()
+
     with TestClient(app) as client:
         yield client
 
@@ -20,9 +27,26 @@ def current_user():
 @pytest.fixture
 def authenticated_client(current_user):
 
-    app.dependency_overrides[get_current_user] = lambda: current_user
-    client = TestClient(app)
-    yield client
+    app.dependency_overrides[get_current_user] = (
+        lambda: current_user
+    )
+
+    fake_org = {
+        "id": str(uuid4()),,
+        "name": "Test Hospital",
+    }
+
+    app.dependency_overrides[
+        require_organization_member
+    ] = lambda: fake_org
+
+    app.dependency_overrides[
+        require_organization_admin
+    ] = lambda: fake_org
+
+    with TestClient(app) as client:
+        yield client
+
     app.dependency_overrides.clear()
 
 
