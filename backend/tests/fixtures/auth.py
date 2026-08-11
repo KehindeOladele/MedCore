25db7fbd4448a04c1df6594fca13ba3b747f37c7
jeffreyesedo <jiesedo@gmail.com>
2026-08-11 15:18:16 +0100

    correct import and calls

--- backend/tests/fixtures/auth.py
+++ backend/tests/fixtures/auth.py
@@ -1,21 +1,30 @@
 import pytest
+
 from fastapi.testclient import TestClient
+
 from main import app
+
 from app.core.security import get_current_user
+
+from app.modules.organizations.dependencies import (
+    require_organization_access,
+    require_organization_admin,
+)
+
 from tests.factories.user import (
     user_factory,
-    USER_ID
-    )
-from app.modules.organizations.dependencies import (
-    require_organization_admin,
-    require_organization_member,
-    )
+    USER_ID,
+)
 
+from tests.factories.organization import (
+    ORGANIZATION_ID,
+)
 
 
 # --------------------------------------
 # CLIENT TEST FIXTURE
 # --------------------------------------
+
 @pytest.fixture
 def client():
     app.dependency_overrides.clear()
@@ -23,34 +32,36 @@ def client():
     with TestClient(app) as client:
         yield client
 
+    app.dependency_overrides.clear()
 
 
 # --------------------------------------
 # CURRENT USER FIXTURE
 # --------------------------------------
+
 @pytest.fixture
 def current_user():
-    return user_factory
-
+    return user_factory()
 
 
 # --------------------------------------
 # AUTHENTICATED CLIENT FIXTURE
 # --------------------------------------
+
 @pytest.fixture
 def authenticated_client(current_user):
+
+    fake_org = {
+        "id": str(ORGANIZATION_ID),
+        "name": "Test Hospital",
+    }
 
     app.dependency_overrides[get_current_user] = (
         lambda: current_user
     )
 
-    fake_org = {
-        "id": str(USER_ID),
-        "name": "Test Hospital",
-    }
-
     app.dependency_overrides[
-        require_organization_member
+        require_organization_access
     ] = lambda: fake_org
 
     app.dependency_overrides[
@@ -62,6 +73,10 @@ def authenticated_client(current_user):
 
     app.dependency_overrides.clear()
 
+
+# --------------------------------------
+# AUTHENTICATED USER
+# --------------------------------------
 
 @pytest.fixture
 def authenticated_user(current_user):
