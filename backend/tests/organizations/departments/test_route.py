db01ba39b20f8fcccafa6f5c9af609f084ca4d31
jeffreyesedo <jiesedo@gmail.com>
2026-08-11 15:48:52 +0100

    correct imports

--- backend/tests/organizations/departments/test_route.py
+++ backend/tests/organizations/departments/test_route.py
@@ -10,11 +10,11 @@ from app.modules.organizations.departments.schemas import (
 )
 from app.modules.organizations.departments.exceptions import (
     DepartmentAlreadyExistsError,
-    DepartmentNotFoundError,
     InvalidParentDepartmentError,
     CircularDepartmentHierarchyError,
     DepartmentHasChildrenError,
 )
+from app.modules.organizations.exceptions import DepartmentNotFoundError
 
 
 
