aefe4307a2142030af7da516075982b676372c9a
jeffreyesedo <jiesedo@gmail.com>
2026-08-13 12:56:01 +0100

    correct router code to follow proper architecture

--- backend/app/modules/organizations/healthcare_services/router.py
+++ backend/app/modules/organizations/healthcare_services/router.py
@@ -11,7 +11,7 @@ from app.core.security import get_current_user
 
 from app.modules.organizations.dependencies import (
     require_organization_admin,
-    require_organization_access
+    require_organization_access,
 )
 
 from .schemas import (
@@ -29,21 +29,20 @@ from .service import (
 )
 
 
-
-
 # -------------------------------------------------------
-# HEALTHCARE SERVIVE API SETUP
+# HEALTHCARE SERVICE API SETUP
 # -------------------------------------------------------
+
 router = APIRouter(
     prefix="/{organization_id}/healthcare-services",
     tags=["Healthcare Services"],
 )
 
 
-
 # -------------------------------------------------------
 # CREATE HEALTHCARE SERVICE ENDPOINT
 # -------------------------------------------------------
+
 @router.post(
     "",
     response_model=HealthcareServiceResponse,
@@ -53,7 +52,7 @@ def create_healthcare_service_endpoint(
     organization_id: UUID,
     payload: HealthcareServiceCreate,
     current_user=Depends(get_current_user),
-    organization: dict = Depends(require_organization_admin),
+    organization=Depends(require_organization_admin),
 ):
     return create_healthcare_service(
         organization_id=organization_id,
@@ -63,8 +62,9 @@ def create_healthcare_service_endpoint(
 
 
 # -------------------------------------------------------
-# LIST HEALTHCARE SERVICE ENDPOINT
+# LIST HEALTHCARE SERVICES ENDPOINT
 # -------------------------------------------------------
+
 @router.get(
     "",
     response_model=list[HealthcareServiceResponse],
@@ -82,8 +82,9 @@ def list_healthcare_services_endpoint(
 # -------------------------------------------------------
 # GET HEALTHCARE SERVICE ENDPOINT
 # -------------------------------------------------------
+
 @router.get(
-    "",
+    "/{healthcare_service_id}",
     response_model=HealthcareServiceResponse,
 )
 def get_healthcare_service_endpoint(
@@ -101,8 +102,9 @@ def get_healthcare_service_endpoint(
 # -------------------------------------------------------
 # UPDATE HEALTHCARE SERVICE ENDPOINT
 # -------------------------------------------------------
+
 @router.patch(
-    "",
+    "/{healthcare_service_id}",
     response_model=HealthcareServiceResponse,
 )
 def update_healthcare_service_endpoint(
@@ -123,8 +125,9 @@ def update_healthcare_service_endpoint(
 # -------------------------------------------------------
 # DELETE HEALTHCARE SERVICE ENDPOINT
 # -------------------------------------------------------
+
 @router.delete(
-    "",
+    "/{healthcare_service_id}",
     status_code=status.HTTP_204_NO_CONTENT,
 )
 def delete_healthcare_service_endpoint(
@@ -139,4 +142,6 @@ def delete_healthcare_service_endpoint(
         actor_id=current_user["id"],
     )
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return Response(
+        status_code=status.HTTP_204_NO_CONTENT
+    )
