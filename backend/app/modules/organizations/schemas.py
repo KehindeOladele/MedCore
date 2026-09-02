99ff6886db0c52c610d92aa6c2bb39a57d980810
jeffreyesedo <jiesedo@gmail.com>
2026-09-02 11:50:44 +0100

    make schemas optional

--- backend/app/modules/organizations/schemas.py
+++ backend/app/modules/organizations/schemas.py
@@ -1,22 +1,43 @@
 from pydantic import (
     BaseModel, 
     EmailStr,
+    Field
 )
 from typing import Optional
 from uuid import UUID
 
 
-# ----------------------------------------
+# ------------------------ ----------------
 #       Organization Models 
 # ----------------------------------------
 class OrganizationBase(BaseModel):
-    name: str
-    type: Optional[str]
-    email: Optional[EmailStr]
-    phone: Optional[str]
-    address: Optional[str]
-    state: Optional[str]
-    country: Optional[str] = "Nigeria"
+    name: str = Field(
+        description= "Organization Name"
+        )
+    type: Optional[str] = Field(
+        default=None,
+        description= "organization type"
+        )
+    email: Optional[EmailStr] = Field(
+        default=None,
+        description="organization email"
+        )
+    phone: Optional[str] = Field(
+        default=None,
+        description="organization phone number"
+        )
+    address: Optional[str] = Field(
+        default=None,
+        description="organization address"
+        )
+    state: Optional[str] = Field(
+        default=None,
+        description="state in country"
+        )
+    country: Optional[str] = Field(
+        default="Nigeria",
+        description="Country of organization"
+        )
 
 
 # ----------------------------------------- 
@@ -31,40 +52,79 @@ class OrganizationCreate(OrganizationBase):
 #           Update Models 
 # ------------------------------------------
 class OrganizationUpdate(BaseModel):
-    name: Optional[str]
-    type: Optional[str]
-    level: Optional[str]
-    email: Optional[EmailStr]
-    phone: Optional[str]
-    address: Optional[str]
-    state: Optional[str]
-    country: Optional[str]
+    name: Optional[str] = Field(
+        description= "Organization Name"
+        )
+    type: Optional[str] = Field(
+        default=None,
+        description= "organization type"
+        )
+    level: Optional[str] = Field(
+        default=None
+    )
+    email: Optional[EmailStr] = Field(
+        default=None,
+        description="organization email"
+        )
+    phone: Optional[str] = Field(
+        default=None,
+        description="organization phone number"
+        )
+    address: Optional[str] = Field(
+        default=None,
+        description="organization address"
+        )
+    state: Optional[str] = Field(
+        default=None,
+        description="state in country"
+        )
+    country: Optional[str] = Field(
+        default="Nigeria",
+        description="Country of organization"
+        )
 
 
 # ------------------------------------------- 
 #         Oganization Logo Models 
 # ------------------------------------------- 
 class OrganizationLogo(OrganizationBase):
-    id: UUID
-    logo_url: Optional[str]
+    id: UUID = Field(
+        description="organization id"
+    )
+    logo_url: Optional[str] = Field(
+        default=None,
+        description="logo image url"
+    )
     
 
 # --------------------------------------------- 
 #           Role Assignment Models 
 # ---------------------------------------------
 class RoleAssignment(BaseModel):
-    user_id: UUID
-    role_name: str
-    org_id: str
+    user_id: UUID = Field(
+        description="user/practitioners id"
+    )
+    role_name: str = Field(
+        description="role name eg. Doctor, Nurse etc."
+    )
+    org_id: str = Field(
+        description="role id"
+    )
 
 
 # ---------------------------------------------- 
 #           Onboarding Invite Models 
 # ----------------------------------------------
 class OnboardingInvite(BaseModel):
-    email: EmailStr
-    role_name: str
-    org_id: str
+    email: EmailStr = Field(
+        description="user/pratitioners email"
+    )
+    role_name: str = Field(
+        description="user/practitioners role"
+    )
+    org_id: str = Field(
+        description="user/practitioners role"
+    )
 
 
 # -------------------------------------------------
