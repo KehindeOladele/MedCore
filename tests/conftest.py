d9e91ce7c87cd8f86fdb9491a7fb9f85f30fc89e
Jeffrey Esedo <65986386+SmogJ@users.noreply.github.com>
2026-09-11 16:30:27 +0100

    Delete tests directory
    Remove Duplicate tests directory for backend tests

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-pytest_plugins = [
-    "tests.fixtures.auth",
-    "tests.fixtures.organizations",
-    "tests.fixtures.departments",
-    "tests.fixtures.healthcare_service"
-]
--- tests/fixtures/supabase.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from unittest.mock import MagicMock
-
-
-@pytest.fixture
-def mock_supabase():
-
-    return MagicMock()
