e9da3de90fda01dbd1187c2efc1920616acccf7f
Jeffrey Esedo <jiesedo@gmail.com>
2026-09-21 12:20:25 +0100

    add fhir canonical base url

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -36,10 +36,13 @@ class Settings:
 
     # ----- Security -----
     JWT_AUDIENCE: str = os.getenv("JWT_AUDIENCE", "authenticated")
-    
+
     # ----- FHIR Configuration -----
     FHIR_VERSION: str = os.getenv("FHIR_VERSION", "R4")
     FHIR_BASE_URL: Optional[str] = os.getenv("FHIR_BASE_URL")
+    FHIR_CANONICAL_BASE_URL: Optional[str] = os.getenv(
+        "FHIR_CANONICAL_BASE_URL"
+    )
 
 
     # ---- Validating Required Settings ----
