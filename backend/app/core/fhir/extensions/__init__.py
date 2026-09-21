944311a1fb5618c523b15aa7f08b061ef3622a6f
Jeffrey Esedo <jiesedo@gmail.com>
2026-09-21 13:27:54 +0100

    export extension packages to init

--- backend/app/core/fhir/extensions/__init__.py
+++ backend/app/core/fhir/extensions/__init__.py
@@ -4,8 +4,26 @@ from app.core.fhir.extensions.definitions import (
 from app.core.fhir.extensions.registry import (
     FHIRExtensionRegistry,
 )
+from app.core.fhir.extensions.catalog import (
+    ORGANIZATION_DESCRIPTION,
+    ORGANIZATION_PRIMARY_COLOR,
+    ORGANIZATION_SECONDARY_COLOR,
+    ORGANIZATION_TIMEZONE,
+    organization_description_extension,
+    organization_primary_color_extension,
+    organization_secondary_color_extension,
+    organization_timezone_extension,
+)
 
 __all__ = [
     "FHIRPrimitiveExtensionDefinition",
     "FHIRExtensionRegistry",
+    "ORGANIZATION_DESCRIPTION",
+    "ORGANIZATION_TIMEZONE",
+    "ORGANIZATION_PRIMARY_COLOR",
+    "ORGANIZATION_SECONDARY_COLOR",
+    "organization_description_extension",
+    "organization_timezone_extension",
+    "organization_primary_color_extension",
+    "organization_secondary_color_extension",
 ]
