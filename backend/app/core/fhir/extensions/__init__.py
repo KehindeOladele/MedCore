dab6a103dbf30afa00c8af787ce37d918b7cde21
Jeffrey Esedo <jiesedo@gmail.com>
2026-09-21 11:41:00 +0100

    export extension definitiona and registry to init

---
+++ backend/app/core/fhir/extensions/__init__.py
@@ -0,0 +1,11 @@
+from app.core.fhir.extensions.definitions import (
+    FHIRPrimitiveExtensionDefinition,
+)
+from app.core.fhir.extensions.registry import (
+    FHIRExtensionRegistry,
+)
+
+__all__ = [
+    "FHIRPrimitiveExtensionDefinition",
+    "FHIRExtensionRegistry",
+]
